43dcfaf37838e809b402dd9d7fe418c35aab3972
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-04-14 21:34:04 +0200

    Do not install Opcache status library

--- provision/provision.sh
+++ provision/provision.sh
@@ -425,18 +425,6 @@ if [[ $ping_result == "Connected" ]]; then
 		echo "phpMemcachedAdmin already installed."
 	fi
 
-	# Checkout Opcache Status to provide a dashboard for viewing statistics
-	# about PHP's built in opcache.
-	if [[ ! -d /srv/www/default/opcache-status ]]; then
-		echo -e "\nDownloading Opcache Status, see https://github.com/rlerdorf/opcache-status/"
-		cd /srv/www/default
-		git clone https://github.com/rlerdorf/opcache-status.git opcache-status
-	else
-		echo -e "\nUpdating Opcache Status"
-		cd /srv/www/default/opcache-status
-		git pull --rebase origin master
-	fi
-
 
 	# Install and configure the latest stable version of WordPress
 	if [[ ! -d /srv/www/wordpress-default ]]; then
